aabf58d2c9f9d0fe70a2d695dcc00e20c20356c5
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-06-18 15:39:24 +0200

    Dokumentiranje endpointa

--- DEV_Test/DEV_Test/Controllers/ProductsController.cs
+++ DEV_Test/DEV_Test/Controllers/ProductsController.cs
@@ -14,13 +14,20 @@ namespace DEV_Test.Controllers
         {
             _productService = productService;
         }
+        // Endpoint vraća listu svih proizvoda (limit dummyJSON je stavljen na 30 tako da vraća prvih 30 proizvoda u listi)
+        // Primjer zahtjeva:
+        // GET https://localhost:7197/api/Products
         [HttpGet]
         public async Task<ActionResult<ResultModel>> GetAllProducts()
         {
             var allProducts = await _productService.GetAllProducts();
             return Ok(allProducts);
         }
-
+        // Endpoint vraća detalje pojedinačnog proizvoda kojeg pretražujemo ovisno o njegovom ID-u.
+        // Primjer zahtjeva:
+        // GET https://localhost:7197/api/Products/5
+        // Endpoint vraća detalje o proizvodu sa ID-om 5. Ukoliko proizvod sa traženim ID-om ne postoji dobit ćemo
+        // 404 odgovor Not Found, odnosno da proizvod sa traženim ID-om ne postoji
         [HttpGet("{id}")]
         public async Task<ActionResult<ResultModel>> GetProductById(int id)
         {
@@ -43,6 +50,12 @@ namespace DEV_Test.Controllers
             return Ok(filteredProduct);
         }
 
+        // Endpoint vraća proizvode ovisno o traženom nazivu. 
+        // Primjerice za pretraživanje riječi Phone pronaći će sve proizvode koji su vezani sa mobitelima
+        // Primjer zahtjeva:
+        // GET https://localhost:7197/api/Products/Search?search=Phone
+        // Endpoint također sprema pretrage za uneseni tekst u bazu podataka, u slučaju da ponovno pretražujemo po istim
+        // pretragama pozvia se na bazu podataka, umjesto da čeka odgovor dummyJSON-a.
         [HttpGet("Search")]
         public async Task<ActionResult<ResultModel>> GetProductsBySearch([FromQuery] SearchRequestDTO searchRequest)
         {
